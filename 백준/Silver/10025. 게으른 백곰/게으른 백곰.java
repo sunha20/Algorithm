bb52ccf8d7a2744a1b761c3df4494eac6dbc49fc
sunha20 <74315791+sunha20@users.noreply.github.com>
2024-07-08 16:42:40 +0900

    [Silver III] Title: 게으른 백곰, Time: 368 ms, Memory: 41440 KB -BaekjoonHub

--- 백준/Silver/10025. 게으른 백곰/게으른 백곰.java
+++ 백준/Silver/10025. 게으른 백곰/게으른 백곰.java
@@ -6,34 +6,25 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         int N = Integer.parseInt(st.nextToken());   // 양동이 개수
-        int K = Integer.parseInt(st.nextToken());   // 움직일 수 있는 거리.
-        int move = 2 * K + 1;
-        int allIce = 0;
-        int len = 0;
+        int K = Integer.parseInt(st.nextToken());   
+        int move = 2 * K + 1;                       // 움직일 수 있는 전체 거리.
+        
         int[] pen = new int[1000001];
+        int len = 0;
+        int sum = 0;
         for (int n=0; n<N; n++) {
             st = new StringTokenizer(br.readLine());
             int ice = Integer.parseInt(st.nextToken());
             int idx = Integer.parseInt(st.nextToken());
             pen[idx] = ice;
-            allIce += ice;
-            if (idx > len) len = idx;
+            if (idx > len) len = idx;               // 유효한 배열 크기 구하기
+            if (idx < move) sum += ice;             // 초기 부분합 구하기
         }
         len++;
 
-        if (len <= move) {
-            System.out.println(allIce);
-            return;
-        }
-
-        int p1 = 0;
-        int p2 = move;
-        int sum = 0;
-        for (int i=p1; i<(p2); i++) {
-            sum += pen[i];
-        }
-
-        int max = sum;
+        int p1 = 0;                         // pointer 1
+        int p2 = move;                      // pointer 2
+        int max = sum;                      // 부분합의 최댓값 구하기.
         for (int i=1; i<(len-(move)); i++) {
             sum -= pen[p1];
             sum += pen[p2];
