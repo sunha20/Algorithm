e1e674a82661c91272f019231db03e68d0be78ac
sunha20 <74315791+sunha20@users.noreply.github.com>
2025-02-27 23:33:41 +0900

    [Silver IV] Title: 비밀번호 찾기, Time: 1016 ms, Memory: 59540 KB -BaekjoonHub

---
+++ 백준/Silver/17219. 비밀번호 찾기/비밀번호 찾기.java
@@ -0,0 +1,28 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        Map<String, String> memo = new HashMap<>();
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());   // 저장한 주소 개수
+        int M = Integer.parseInt(st.nextToken());   // 찾을 주소 개수
+
+        // 주소 저장
+        for (int n=0; n<N; n++) {
+            st = new StringTokenizer(br.readLine());
+            memo.put(st.nextToken(), st.nextToken());
+        }
+
+        // 주소 검색
+        for (int m=0; m<M; m++) {
+            System.out.println(memo.get(br.readLine()));
+        }
+    }
+}
