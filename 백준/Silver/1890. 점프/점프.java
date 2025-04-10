ebcba8d13569943b2e0141c3aea05dffec6f0b5c
sunha20 <74315791+sunha20@users.noreply.github.com>
2025-04-10 15:28:18 +0900

    [Silver I] Title: 점프, Time: 112 ms, Memory: 14284 KB -BaekjoonHub

--- 백준/Silver/1890. 점프/점프.java
+++ 백준/Silver/1890. 점프/점프.java
@@ -1,42 +1,31 @@
 import java.io.*;
-import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
-    static int N;
-    static int[][] game;
-    static long[][] dp;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        N = Integer.parseInt(br.readLine()) + 1;
-        game = new int[N][N];
-        dp = new long[N][N];
+        int N = Integer.parseInt(br.readLine());
+        int[][] game = new int[N][N];
+        long[][] dp = new long[N][N];
         StringTokenizer st;
-        for (int i = 1; i < N; i++) {
+        for (int i = 0; i < N; i++) {
             st = new StringTokenizer(br.readLine());
-            for (int j = 1; j < N; j++) {
+            for (int j = 0; j < N; j++) {
                 game[i][j] = Integer.parseInt(st.nextToken());
-                dp[i][j] = -1;
             }
         }
+        dp[0][0] = 1;
 
-        System.out.println(recursive(1, 1));
-    }
-
-    private static long recursive(int x, int y) {
-        if (x == N - 1 && y == N - 1) {
-            return 1;
+        for (int x = 0; x < game.length; x++) {
+            for (int y = 0; y < game.length; y++) {
+                int jump = game[x][y];
+                if (jump == 0) continue;
+                if (x+jump < N) dp[x+jump][y] += dp[x][y];
+                if (y+jump < N) dp[x][y+jump] += dp[x][y];
+            }
         }
-
-        if (x >= N || y >= N || game[x][y] == 0) {
-            return 0;
-        }
-
-        if (dp[x][y] == -1) {
-            dp[x][y] = recursive(x + game[x][y], y) + recursive(x, y + game[x][y]);
-        }
-        return dp[x][y];
+        System.out.println(dp[N-1][N-1]);
     }
 
 }
