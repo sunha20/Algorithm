a7365eb7dbfbd85675f923ed7305d5e8e465edcd
sunha20 <74315791+sunha20@users.noreply.github.com>
2025-05-16 23:13:24 +0900

    [Bronze II] Title: 소수 찾기, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/1978. 소수 찾기/소수 찾기.py
+++ 백준/Bronze/1978. 소수 찾기/소수 찾기.py
@@ -1,12 +1,10 @@
-import math
-
 N = int(input())
 nums = list(map(int, input().split()))
 cnt = 0
 
 for n in nums:
     if n == 1: cnt -= 1
-    for i in range(2, int(math.sqrt(n))+1):
+    for i in range(2, int(n**(0.5))+1): # math.sqrt 안하고 이거 해도 되네!
         if n % i == 0:
             break
     else:
