68ce62b950ece317e7e53863a25dc0ddcb073031
sunha20 <74315791+sunha20@users.noreply.github.com>
2025-05-19 10:56:06 +0900

    [Silver IV] Title: 한수, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/1065. 한수/한수.py
+++ 백준/Silver/1065. 한수/한수.py
@@ -1,40 +1,19 @@
-import sys
+N = int(input())
 
+if N < 100:
+    print(N)
+else:
+    if N == 1000: N -= 1
 
-def solution():
-    x = int(sys.stdin.readline().rstrip())
+    sum = 99
+    numList = [0,0,0]
 
-    if x <= 99:
-        return(x)
-    
-    n = 10
-    d = -1
-    a_1 = 0
-    result = 99
-    i = 1
-    
-    while n < x//(10**i):
-        if a_1+d >= 0 and a_1+d<=9:
-            result+=1
-        n+=1
-        if n == 100:
-            n = 10
-            flag = x//100
-            i = 2
-        d = ((n%10) - (n//10))*i
-        a_1 = n%10
+    for num in range(100, N+1):
+        for i in range(3):
+            numList[i] = num%10
+            num //= 10
 
-    n_str = str(n)
+        if numList[0] - numList[1] == numList[1] - numList[2]:
+            sum += 1
 
-    if a_1+d >= 0 and a_1+d<=9:
-        for j in range(1,i+1):
-            d = ((n%10) - (n//10))*j        
-            n_str += str((d*j)+a_1)
-
-        if int(n_str) <= x:
-            result+=1
-
-    return result  
-
-if __name__ == "__main__":
-    print(solution())
+    print(sum)
