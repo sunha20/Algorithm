7818487419231c1db8467be3a939bdff1d9d9a0d
sunha20 <74315791+sunha20@users.noreply.github.com>
2025-05-20 22:54:11 +0900

    [Gold IV] Title: N-Queen, Time: 20840 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Gold/9663. N－Queen/N－Queen.py
+++ 백준/Gold/9663. N－Queen/N－Queen.py
@@ -2,14 +2,14 @@ def n_queen(i):
     if (i == N): return 1
     cnt = 0
     for j in range(N):
-        if checkCol[j] == False and checkDiagAdd[i+j] == False and checkDiagMinus[i-j+N] == False:
-            checkCol[j] = checkDiagAdd[i+j] = checkDiagMinus[i-j+N] = True
+        if checkCol[j] == 0 and checkDiagAdd[i+j] == 0 and checkDiagMinus[i-j+N] == 0:
+            checkCol[j] = checkDiagAdd[i+j] = checkDiagMinus[i-j+N] = 1
             cnt += n_queen(i+1)
-            checkCol[j] = checkDiagAdd[i+j] = checkDiagMinus[i-j+N] = False
+            checkCol[j] = checkDiagAdd[i+j] = checkDiagMinus[i-j+N] = 0
     return cnt
 
 N = int(input())
-checkCol = [False] * N
-checkDiagAdd = [False] * (2*N)
-checkDiagMinus = [False] * (2*N)
+checkCol = [0] * N
+checkDiagAdd = [0] * (2*N)
+checkDiagMinus = [0] * (2*N)
 print(n_queen(0))
