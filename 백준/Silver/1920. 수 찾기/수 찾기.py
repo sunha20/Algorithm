ca9ecb988d28faa1c7e942cdb869b8b56ccbf273
sunha20 <74315791+sunha20@users.noreply.github.com>
2025-05-24 14:13:54 +0900

    [Silver IV] Title: 수 찾기, Time: 728 ms, Memory: 48592 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.py
+++ 백준/Silver/1920. 수 찾기/수 찾기.py
@@ -1,3 +1,4 @@
+# B
 N = int(input())
 lst = list(map(int, input().split(" ")))
 lst.sort()
@@ -6,14 +7,14 @@ targets = list(map(int, input().split(" ")))
 
 for t in targets:
     si = 0
-    ei = N -1
-    while si <= ei:
+    ei = N
+    while si < ei:
         mid = (si + ei) // 2
         if lst[mid] == t:
             print(1)
             break
         else:
             if lst[mid] < t: si = mid + 1
-            else: ei = mid - 1
+            else: ei = mid
     else:
         print(0)
