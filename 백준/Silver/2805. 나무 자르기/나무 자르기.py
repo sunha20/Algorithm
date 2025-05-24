51418d86ec842199c51f2cef70f5ad14c1341503
sunha20 <74315791+sunha20@users.noreply.github.com>
2025-05-24 17:54:50 +0900

    [Silver II] Title: 나무 자르기, Time: 2856 ms, Memory: 145580 KB -BaekjoonHub

--- 백준/Silver/2805. 나무 자르기/나무 자르기.py
+++ 백준/Silver/2805. 나무 자르기/나무 자르기.py
@@ -4,7 +4,7 @@ trees.sort()
 hl = 0
 hr = trees[-1]
 while hl<hr:
-    hm = (hl + hr+1) // 2
+    hm = (hl + hr) // 2
     idx = 0
     for t in range(N):
         if hm < trees[t]:
@@ -12,7 +12,7 @@ while hl<hr:
             break
 
     m = sum(trees[idx:]) - (hm*(N-idx))
-    if m < M: hr = hm-1
-    if m >= M: hl = hm
+    if m < M: hr = hm
+    if m >= M: hl = hm + 1
 
-print(hl)
+print(hr-1)
