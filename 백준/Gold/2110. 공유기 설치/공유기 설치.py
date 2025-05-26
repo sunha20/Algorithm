47a2b8e6d05ef7eda1f6c40364693b7c788888fe
sunha20 <74315791+sunha20@users.noreply.github.com>
2025-05-26 22:08:08 +0900

    [Gold IV] Title: 공유기 설치, Time: 5188 ms, Memory: 41164 KB -BaekjoonHub

---
+++ 백준/Gold/2110. 공유기 설치/공유기 설치.py
@@ -0,0 +1,25 @@
+N, C = map(int, input().split())
+homes = [int(input()) for _ in range(N)]
+homes.sort()
+
+dl = 1
+dr = (homes[-1] - homes[0])+1
+
+while dl < dr:
+    cnt = C - 1
+    dm = (dr + dl) // 2
+
+    # 설치
+    i = 0
+    for j in range(i, N):
+        d = homes[j] - homes[i]
+        if d >= dm:
+            cnt -= 1
+            i = j
+
+    if cnt <= 0:        # 설치가 많이 되었으면
+        dl = dm + 1     # 거리를 더 늘려야함
+    else:               # 설치가 적게 되었으면
+        dr = dm         # 거리를 줄여야함.
+
+print(dr-1)
