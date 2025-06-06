5fccb2dc2416b3bc45d4bd0eea11906f0ffb0996
sunha20 <74315791+sunha20@users.noreply.github.com>
2025-06-06 16:35:32 +0900

    [Bronze I] Title: 피보나치 수 2, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/2748. 피보나치 수 2/피보나치 수 2.py
+++ 백준/Bronze/2748. 피보나치 수 2/피보나치 수 2.py
@@ -1,3 +1,6 @@
+"""
+### bottom-up
+### 작은 문제부터 큰문제로 쌓아 올림.
 N = int(input())
 dp = [0] * (N+1)
 dp[0] = 0
@@ -7,3 +10,22 @@ for i in range(2, N+1):
     dp[i] = dp[i-1] + dp[i-2]
 
 print(dp[N])
+"""
+
+### top-down
+### 큰 문제를 작은 문제로 재귀 호출
+### 그 작은 문제가 이미 계산 한 문제면 가지고 옴
+
+def fib(n):
+    if dp[n-1] == -1:
+        dp[n-1] = fib(n-1)
+    if dp[n-2] == -1:
+        dp[n-2] = fib(n-2)
+    return dp[n-1] + dp[n-2]
+
+N = int(input())
+dp = [-1] * (N+1)
+dp[0] = 0
+dp[1] = 1
+
+print(fib(N))
