479350d66a6dd2f1695ee248bb160e4be4eba457
sunha20 <74315791+sunha20@users.noreply.github.com>
2025-06-07 01:29:46 +0900

    [Gold V] Title: LCS, Time: 456 ms, Memory: 57284 KB -BaekjoonHub

--- 백준/Gold/9251. LCS/LCS.py
+++ 백준/Gold/9251. LCS/LCS.py
@@ -1,24 +1,18 @@
-import sys
-sys.setrecursionlimit(10**6)
-
-def lcs(s1, s2):
-    if s1 >= l1 or s2 >= l2:
-        return 0
-
-    if dp[s1][s2] >= 0:
-        return dp[s1][s2]
-
-    if str1[s1] == str2[s2]:
-        dp[s1][s2] = lcs(s1+1, s2+1) + 1
-    else:
-        dp[s1][s2] = max(lcs(s1+1, s2), lcs(s1, s2+1))
-
-    return dp[s1][s2]
-
 str1 = input()
 str2 = input()
 l1 = len(str1)
 l2 = len(str2)
-dp = [[-1 for _ in range(l2)] for _ in range(l1)]
+dp = [[-1 for _ in range(l2+1)] for _ in range(l1+1)]
+dp[0] = [0 for _ in range(l2+1)]
+for i in range(l1+1):
+    dp[i][0] = 0
 
-print(lcs(0,0))
+# 이해가 쉽게 앞에서 부터 쌓아가자.
+# lcs(n1, n2) = max(lcs(n1-1, n2-1) + 1, lcs(n1-1,n2), lcs(n1, n2-1))
+for n1 in range(1, l1+1):
+    for n2 in range(1, l2+1):
+        if str1[n1-1] == str2[n2-1]:
+            dp[n1][n2] = dp[n1-1][n2-1] + 1
+        else:
+            dp[n1][n2] = max(dp[n1-1][n2], dp[n1][n2-1])
+print(dp[l1][l2])
